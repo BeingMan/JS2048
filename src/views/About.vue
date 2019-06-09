613d6db7f1a8718aa81ca668cb43e5696d89f61e
31601411 软工1602 叶柏成 <l>
2019-06-09 21:02:03 +0800

    增加按键监听事件，实现数组移动，重置

--- src/views/About.vue
+++ src/views/About.vue
@@ -23,15 +23,15 @@
           <el-header>
             <el-row>
               <el-col :span="6">
-                <el-button type="primary" round @click="transArray">开始</el-button>
+                <el-button type="primary" round @click="moveBox">开始</el-button>
               </el-col>
               <el-col :span="6">
-                <el-button type="primary" round @click="randomAdd">重置</el-button>
+                <el-button type="primary" round @click="reGame">重置</el-button>
               </el-col>
             </el-row>
           </el-header>
           <el-main>
-            <el-row type="flex" justify="certain" v-for="items in changeNums">
+            <el-row type="flex" justify="certain" v-for="items in nums">
               <el-col :span="1" v-for="item in 4" class="box">{{items[item-1]}}</el-col>
             </el-row>
           </el-main>
@@ -46,11 +46,23 @@
       return {
         nums: [
           [2, 2, 4, 4],
-          ['', '', '', ''],
-          ['', '', '', ''],
-          ['', '', '', '']
+          [2, 2, 4, 4],
+          [2, 2, 4, 4],
+          [2, 2, 4, 4]
         ]
       }
+    },
+    created() {
+      document.addEventListener('keyup', function (e) {
+        const map = {
+          37: 0,
+          38: 3,
+          39: 2,
+          40: 1
+        };
+        if (!(e.keyCode in map)) return;
+        this.handleMove(map[e.keyCode]);
+      })
     },
     methods: {
       transArray() {
@@ -67,18 +79,59 @@
         while (!add) {
           x = Math.round(3 * Math.random());
           y = Math.round(3 * Math.random());
-          console.log("x:" + x + "y:" + y);
           if (this.nums[x][y] === '') {
             add = true;
             this.$set(this.nums[x], y, Math.random() > 0.9 ? 4 : 2);
-            console.log(this.nums);
           }
         }
-      }
-    },
-    computed: {
-      changeNums() {
-        return this.nums;
+      },
+      handleMove(times) {
+
+      },
+      reGame() {
+        var arr = new Array();
+        for (var x = 0; x < 4; x++) {
+          arr[x] = new Array();
+          for (var y = 0; y < 4; y++) {
+            arr[x][y] = '';
+          }
+        }
+        this.nums = arr;
+        for (var i = 0; i < 2; i++) {
+          this.randomAdd();
+        }
+      },
+      moveBox() {
+        var oldArray = this.nums;
+        var newArray = [];
+        var _this = this;
+        oldArray.forEach((elements,index) => {
+          var newlist = elements;
+          var Merges = new Array(4).fill(false);
+          elements.forEach((element, index) => {
+            var isMerge = false;
+            var newIndex = index;
+            var newNum = element;
+            for (var i = index-1; i >=0 ; i--) {
+              if(newlist[i]===''){
+                newlist[newIndex] = '';
+                newIndex = i;
+              }else if(Merges[i]){
+                break;
+              }else{
+                newlist[newIndex] = '';
+                newNum = 2*element;
+                newIndex = i;
+                isMerge = true;
+                break;
+              }
+            }
+            Merges[newIndex] = isMerge;
+            newlist[newIndex] =newNum;
+          })
+          this.$set(this.nums,index,newlist);
+        });
+       
       }
     }
   }
