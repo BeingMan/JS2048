3e2b5b20772c5b48a1545becad5bd703a64b64f2
31601411 软工1602 叶柏成 <l>
2019-06-09 16:40:36 +0800

    实现登录跳转主页，主页界面显示

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,74 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+  <el-container>
+    <el-header>
+      <el-row>
+        <el-col :span="6" :offset="11">
+          2048
+        </el-col>
+      </el-row>
+    </el-header>
+    <el-container>
+      <el-aside style="background-color: aqua">
+        <el-container>
+          <el-header>
+            排行榜标题
+          </el-header>
+          <el-main>
+            排行榜内容
+          </el-main>
+        </el-container>
+      </el-aside>
+      <el-main style="background-color: gray">
+        <el-container>
+          <el-header>
+            <el-row>
+              <el-col :span="6">
+                <el-button type="primary" round @click="transArray">开始</el-button>
+              </el-col>
+              <el-col :span="6">
+                <el-button type="primary" round>重置</el-button>
+              </el-col>
+            </el-row>
+          </el-header>
+          <el-main>
+            <el-row type="flex" justify="certain" v-for="items in nums">
+              <el-col :span="1" v-for="item in 4" class="box" v-model="nums">{{items[item-1]}}</el-col>
+            </el-row>
+          </el-main>
+        </el-container>
+      </el-main>
+    </el-container>
+  </el-container>
 </template>
+<script>
+  export default {
+    data() {
+      return {
+        nums: [
+          [1, 2, 3, 4],
+          [5, 6, 7, 8],
+          [9, 10, 11, 12],
+          [13, 14, 15, 16]
+        ]
+      }
+    },
+    methods: {
+      transArray() {
+        var newArray = this.nums[0].map(function (col, i) {
+          return this.nums.map(function (row) {
+            return row[i];
+          })
+        }, this);
+        this.nums = newArray;
+      }
+    },
+  }
+</script>
+
+<style scoped>
+  .box {
+    text-align: center;
+    height: 40px;
+    line-height: 40px
+  }
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,53 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <el-row>
+      <el-col :span="24" :offset="11">2048小游戏</el-col>
+    </el-row>
+    <el-row>
+      <el-col :span="4" :offset="10">
+        <el-input placeholder="请输入账号" type="text" name="username" v-model="userName"></el-input>
+      </el-col>
+    </el-row>
+    <el-row>
+      <el-col :span="4" :offset="10">
+        <el-input placeholder="请输入密码" show-password type="text" name="password" v-model="password"></el-input>
+      </el-col>
+    </el-row>
+    <el-row>
+      <el-col :span="1" :offset="10">
+        <el-button type="primary" plain>注册</el-button>
+      </el-col>
+      <el-col :span="1" :offset="2">
+        <!-- <router-link to="/about"> -->
+          <el-button type="primary" plain @click="addUser">登录</el-button>
+        <!-- </router-link> -->
+      </el-col>
+    </el-row>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
-export default {
-  name: 'home',
-  components: {
-    HelloWorld
+  export default {
+    data() {
+      return {
+        userName: '',
+        password: ''
+      }
+    },
+    methods: {
+      addUser() {
+        var name = this.userName
+        var password = this.password
+        this.$http.post('/api/user/addUser', {
+          username: name,
+          age: password
+        }, {}).then((response) => {
+          console.log(response)
+          this.$router.push({path:'/about'});
+        }).catch(()=>{
+          
+        })
+      }
+    }
   }
-}
 </script>
